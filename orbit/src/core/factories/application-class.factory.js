7925e84aefbcd85c9ab9c06174b4935e717a25f2
Caio Vaccaro <caiovaccaromora@gmail.com>
2016-02-21 16:16:32 -0300

    AMD converted into ES6 syntax

--- orbit/src/core/factories/application-class.factory.js
+++ orbit/src/core/factories/application-class.factory.js
@@ -1,46 +1,47 @@
-define('OrbitClass', ['stampit', '../mediator/channel'], function(stampit, OrbitMediator) {
-	var publicClassFactory = {
-		extend: function(options) {
-			var stamp = stampit({
-				props: options.props,
-				methods: options.methods
-			});
+import stampit from 'stampit';
+import OrbitMediator from '../mediator/channel';
 
-			return stampit.compose(stamp, internalClassFactory(options.actions))();
-		}
-	};
-
-	function internalClassFactory(actions) {
-		return stampit().init(function(construct) {
-			var instance = construct.instance,
-				action,
-				i;
-
-			instance.actions = actions;
-			registerActions(actions, instance);
+let publicClassFactory = {
+	extend: function(options) {
+		let stamp = stampit({
+			props: options.props,
+			methods: options.methods
 		});
+
+		return stampit.compose(stamp, internalClassFactory(options.actions))();
 	}
+};
 
-	function registerActions(actions, instance) {
-		for (action in actions) {
-			if (typeof instance[actions[action]] === 'function') {
-				OrbitMediator.subscribe({
-					topic: actions[action],
-					callback: function(data, envelope) {
-						var response, error;
+function internalClassFactory(actions) {
+	return stampit().init(function(construct) {
+		let instance = construct.instance,
+			action,
+			i;
 
-						try {
-							response = instance[actions[action]](data);
-						} catch(e) {
-							error = e;
-						} finally {
-							envelope.reply(error, response);
-						}
+		instance.actions = actions;
+		registerActions(actions, instance);
+	});
+}
+
+function registerActions(actions, instance) {
+	for (action in actions) {
+		if (typeof instance[actions[action]] === 'function') {
+			OrbitMediator.subscribe({
+				topic: actions[action],
+				callback: function(data, envelope) {
+					let response, error;
+
+					try {
+						response = instance[actions[action]](data);
+					} catch(e) {
+						error = e;
+					} finally {
+						envelope.reply(error, response);
 					}
-				});
-			}
+				}
+			});
 		}
 	}
+}
 
-	return publicClassFactory;
-});
+export default publicClassFactory;
--- orbit/src/core/mediator/channel.js
+++ orbit/src/core/mediator/channel.js
@@ -1,4 +1,8 @@
-define('Mediator', ['postal', 'postal.request-response', 'q'], function(postal, rp, Q) {
+import postal from 'postal';
+import 'postal.request-response';
+import Q from 'q';
+
+export default function() {
     postal.configuration.promise.createDeferred = function() {
         return Q.defer();
     };
@@ -8,4 +12,4 @@ define('Mediator', ['postal', 'postal.request-response', 'q'], function(postal, 
     };
 
     return postal.channel('ORBIT');
-});
+}
