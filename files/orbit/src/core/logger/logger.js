08f9af1f12510e88c6814564461a8046bfe94104
Caio Vaccaro <caiovaccaromora@gmail.com>
2016-03-03 21:04:42 -0300

    Better Symbol organization if loggin levels

--- files/orbit/src/core/logger/logger.js
+++ files/orbit/src/core/logger/logger.js
@@ -1,20 +1,30 @@
+const OFF = Symbol('No logs will be shown'),
+	FATAL = Symbol('Only errors that will break your app will be shown'),
+	ERROR = Symbol('Any error will be logged'),
+	WARN = Symbol('Errors and warnings will be shown'),
+	INFO = Symbol('Errors, warnings and relevant information will be shown'),
+	ALL = Symbol('Everything will be logged and displayed on the console');
+
 const levels = {
-	OFF: Symbol('No logs will be shown'),
-	FATAL: Symbol('Only errors that will break your app will be shown'),
-	ERROR: Symbol('Any error will be logged'),
-	WARN: Symbol('Errors and warnings will be shown'),
-	INFO: Symbol('Errors, warnings and relevant information will be shown'),
-	ALL: Symbol('Everything will be logged and displayed on the console')
+	OFF, 
+	FATAL, 
+	ERROR, 
+	WARN, 
+	INFO, 
+	ALL
 };
 
 let Logger = {
 	level: levels['OFF'],
-	log: log,
-	parse: parse
+	log: log
 };
 
-function log() {}
+function log(message, level) {
+	if(this.levels[level] === this.level) this.output(message);
+}
 
-function parse() {}
+function output(message) {
+	console.log(message);
+}
 
 export default Logger;
