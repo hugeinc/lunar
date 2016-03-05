d6e1b73f0f2b41526a29aa4d2e8562b436756f15
Caio Vaccaro <caiovaccaromora@gmail.com>
2016-03-05 13:55:35 -0300

    Dispatcher logs.

--- files/orbit/src/core/factories/dispatcher.factory.js
+++ files/orbit/src/core/factories/dispatcher.factory.js
@@ -1,5 +1,6 @@
 import ActionEmitter from '../factories/action-emitter.factory';
 import ViewProvider from '../factories/view-provider.factory';
+import Logger from '../logger/logger';
 import _ from 'lodash';
 
 function extend(actions, middlewares) {
@@ -7,14 +8,18 @@ function extend(actions, middlewares) {
     dispatcher = {},
 		middleware;
 
+	Logger.log({ message: '[Dispatcher.extend] Extending ActionEmitter.', level: 'ALL' });
+
   _.extend(emitter, ActionEmitter.extend(actions));
 
   if (typeof middlewares !== 'undefined' && middlewares.length) {
     for (middleware in middlewares) {
+    	Logger.log({ message: `[Dispatcher.extend] Adding middleware ${middlewares[middleware].toString()}`, level: 'ALL' });
       emitter.addMiddleware(middlewares[middleware]);
     }
   }
 
+  Logger.log({ message: '[Dispatcher.extend] Extending ViewProvider.', level: 'ALL' });
   _.extend(dispatcher, ViewProvider.extend([emitter.service]));
 
   return dispatcher;
